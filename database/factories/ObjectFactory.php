d59f7bf58e52b5d7e03a78f6ef8457fe5f965e0b
Sergi Virgili <svirgilif@gmail.com>
2019-10-17 14:05:01 +0200

    OBJECTS CREUD INIT

---
+++ database/factories/ObjectFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Object;
+use Faker\Generator as Faker;
+
+$factory->define(Object::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
